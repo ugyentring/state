15347ff753ea8fc7cd395f92ba1b1187a1dbfdb3
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-04-08 11:04:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState();
+
+  return (
+    <>
+      <div className="App">
+        <div className="box">
+          
+        </div>
+    </div>
+    </>
+  );
+};
+
+export default App;
