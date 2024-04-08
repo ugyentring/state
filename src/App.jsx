2ec233c3ad2edc11c7b4270a066d7597e6e311bf
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-04-08 11:40:35 +0600

    done with the state handle in counter program

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import { useState } from "react";
+import "./App.css";
 
 const App = () => {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(0);
+
+  function handleAdd() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function handleSub() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  function handleReset() {
+    setCount(0);
+  }
 
   return (
     <>
       <div className="App">
         <div className="box">
-          
+          <p>{count}</p>
+          <button onClick={handleAdd} className="add">
+            Add
+          </button>
+          <button onClick={handleSub} className="sub">
+            Sub
+          </button>
+          <br />
+          <button onClick={handleReset} className="reset">
+            Reset
+          </button>
         </div>
-    </div>
+      </div>
     </>
   );
 };
